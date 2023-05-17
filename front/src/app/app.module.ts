9dbf8e24eafe73511a70944bc1a8260e09e94363
Ghazaleh Hadian <ghazal.hadiyan@gmail.com>
2023-05-17 23:20:35 +0200

    create people component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { LoadingComponent } from './loading/loading.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainModule } from './modules/main/main.module';
 import { RoutingModule } from './modules/routing/routing.module';
+import { PeopleComponent } from './people/people.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    PeopleComponent,
   ],
   imports: [BrowserModule, RoutingModule, MainModule,],
   providers: [],
